b64f64d02c978d8f2435b32ee448051a41252615
Anuragy Singh Yadav <anuragysinghyadavv@gmail.com>
2026-04-28 02:37:35 +0530

    adding project on git hub

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodoverride = require("method-override")
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/expresserror.js");
+const wrapAsync = require("./utils/wrapAsync.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+main()
+.then(() =>{
+    console.log("connected to DB");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended :true}))
+app.use(methodoverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")))
+
+app.get("/", (req,res) =>{
+  res.send("Home Page");
+});
+
+
+app.get("/listings", wrapAsync(async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index", { allListings });
+}));
+
+// New Route — must come before Show Route
+app.get("/listings/new", wrapAsyncasync((req, res) => {
+  res.render("listings/new");
+}));
+
+// Show Route
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show", { listing });
+}));
+
+// Create Route
+app.post("/listings",
+   wrapAsync(async (req,res) => {
+  if(!req.body.listing) {
+    throw new ExpressError(400,"Send some valid data to the server");
+  }
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+}));
+
+
+//Edit route
+app.get("/listings/:id/edit", wrapAsync(async(req,res) => {
+   let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", {listing});
+}));
+
+//Update Route
+app.put("/listings/:id", wrapAsync(async(req,res) => {
+  let {id} = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  res.redirect(`/listings/${id}`);
+}));
+
+//Delete Route
+app.delete("/listings/:id",wrapAsync(async (req,res) => {
+  let {id} = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+}));
+
+app.all("*", (req,res,next)=> {
+  next(new ExpressError(404, "Page Not Found!"))
+})
+
+app.use((err,req,res,next) => {
+  let{statusCode=500, message="Something went wrong"} = err
+  res.status(statusCode).send(message);
+});
+
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema ({
+    title:{
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+    filename: String,
+    url: {
+      type: String,
+      default: "https://unsplash.com/photos/aerial-view-of-a-bustling-city-street-at-dusk-CdFRT5-_AXg",
+      set: (v) =>
+        v === ""
+          ? "https://unsplash.com/photos/aerial-view-of-a-bustling-city-street-at-dusk-CdFRT5-_AXg"
+          : v,
+    },
+  },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+
+
+const Listing = mongoose.model("Listing", listingSchema)
+module.exports = Listing;
