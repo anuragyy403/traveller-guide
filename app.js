3ddb82d05fb6999480998a61cb8b4bd6dd22bf9c
anuragyy403 <anuragysinghyadavv@gmail.com>
2026-06-08 13:07:06 +0530

    change the route of the homepage to the listings page

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(express.static(path.join(__dirname, "/public")))
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -64,7 +64,7 @@ store.on("error", (err) => {
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: "process.env.SECRET",
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -111,7 +111,7 @@ app.use((req, res, next) => {
 
 
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  res.redirect("/listings");
 });
 
 
